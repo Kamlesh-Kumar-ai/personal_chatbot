188b1038e99dadc11d2e0bdc26d7ad6b8b64ee53
@kamlesh <kk919844@gmail.com>
2026-06-17 12:30:01 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 # from openai import OpenAI
 
-# client = OpenAI(api_key="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")
+# client = ")
 
 # response = client.responses.create(
 #     model="gpt-5",
